1d5b4ffaf1b15abf9eff544a72bee6cdee2147dc
谭俊杰 <tanjunjie@tzt.cn>
2018-11-07 14:39:40 +0800

    提交组合模式的代码

---
+++ MediatorModule/src/main/java/com/example/mediatormodule/Dir.java
@@ -0,0 +1,59 @@
+package com.example.mediatormodule;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Dir {
+
+    /*
+    * 声明一个list成员变量存储文件夹下的所有元素
+    *
+    * */
+    protected List<Dir> dirs = new ArrayList<>();
+
+    private String name;
+
+    public Dir(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /*
+    * 添加一个文件或文件夹
+    *
+    * */
+    public abstract void addDir(Dir dir);
+
+    /*
+    * 移除一个文件夹
+    *
+    * */
+    public abstract void removeDir(Dir dir);
+
+    /*
+    * 清除文件夹下的所有元素
+    *
+    * */
+    public abstract void clear();
+
+    /*
+    * 输入文件夹下所有元素
+    *
+    * */
+    public abstract void print();
+
+    /*
+    * 获取文件或文件名
+    *
+    * */
+    public abstract List<Dir> getFiles();
+
+
+}
